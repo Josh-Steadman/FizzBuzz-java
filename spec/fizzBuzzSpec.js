350bdc6fab8df72c503bbde9d6dca35c7008d4d8
joshSteadman <josh.steadman15@gmail.com>
2020-09-01 17:45:40 +0100

    test for dividing by three passed

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -6,10 +6,16 @@ describe('Fizzscript', function() {
     describe('it is divisable by', function() {
 
         it('divisable by 3', function() {
-          fizzscript =  new Fizzscript(); 
-          expect(isDivisableByThree(3)).toBe(true);
+          fizzscript =  new Fizzscript() 
+          expect(fizzscript.isDivisableByThree(3)).toBe(true);
 
         });
+
+        it('not divisable by 3', function() {
+            fizzscript =  new Fizzscript() 
+            expect(fizzscript.isDivisableByThree(4)).toBe(false);
+        });
+  
     });
 
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,1 +1,10 @@
  
+ class Fizzscript {
+     isDivisableByThree(num) {
+         if (num % 3 === 0) {
+           return true
+         } else {
+           return false
+         }
+     }
+ }
